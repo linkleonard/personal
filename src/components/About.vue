7fbe0330e150b30d2eabde07b657aa41ce7bb8d4
Leonard Law <leon.wlaw@gmail.com>
2017-11-15 02:34:43 -0500

    Add link to github
    Also move global styles to App component

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,6 +2,9 @@
   <div>
     <heading>About</heading>
     <div>{{ roles }}</div>
+    <ul>
+      <li>GitHub: <a href="https://github.com/linkleonard">https://github.com/linkleonard</a></li>
+    </ul>
   </div>
 </template>
 
