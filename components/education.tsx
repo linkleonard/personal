b0c63c7b7209c95611e9b1f488f755ef10940886
Leonard <leon.wlaw@gmail.com>
2022-08-05 19:04:54 -0400

    Styling for education

--- components/education.tsx
+++ components/education.tsx
@@ -1,12 +1,16 @@
+import styles from "../styles/Education.module.css";
+
 const Education = () => (
   <ul>
-    <li>
-      <h3>
-        NYU Tandon School of Engineering{" "}
+    <li className={styles.enrollment}>
+      <div className={styles.school}>
+        <h3>NYU Tandon School of Engineering </h3>
         <i>formerly Polytechnic Institute of New York University</i>
-      </h3>
-      <time>September 2010</time>-<time>May 2014</time>
-      <p>Bachelor of Science, Computer Science</p>
+      </div>
+      <span className={styles.period}>
+        <time>September 2010</time>-<time>May 2014</time>
+      </span>
+      <p className={styles.title}>Bachelor of Science, Computer Science</p>
       <h4>Honors</h4>
       <ul>
         <li>Magna Cum Laude (3.767 GPA)</li>
