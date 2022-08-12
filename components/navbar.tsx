8c2c8a8c7377bf63b36000cc2df97a49d7ca603b
Leonard <leon.wlaw@gmail.com>
2022-08-12 00:25:14 -0400

    Move links to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import global from "../styles/Global.module.css";
 import styles from "../styles/Nav.module.css";
 
 const NavBar = () => (
   <nav className={styles.nav}>
-    <h1>Leonard Law</h1>
+    <h1 className={styles.name}>Leonard Law</h1>
     <Link href="/">About</Link>
     <Link href="/resume">Resume</Link>
-    <Link href="/contact">Connect</Link>
+    <a
+      className={global.outbound}
+      href="https://www.linkedin.com/in/leonard-law/"
+    >
+      LinkedIn
+    </a>
+    <a className={global.outbound} href="https://github.com/linkleonard">
+      GitHub
+    </a>
   </nav>
 );
 
