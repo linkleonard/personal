cc043db30934ff34c07825a16bf709a1f31ae643
Leonard <leon.wlaw@gmail.com>
2022-08-13 02:37:59 -0400

    Fix footer styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,5 @@
-import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
-import NavBar from "../components/navbar";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => (
   <footer className={styles.footer}>
