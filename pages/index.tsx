6736185d20557b33e9d077a1f9f383d69381fb69
Leonard <leon.wlaw@gmail.com>
2022-08-17 20:57:38 -0400

    Restore homepage styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Footer from "../components/site/footer";
 import NavBar from "../components/site/navbar";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
